0f31e82f4512d04f25bedfa449323fe0344b080c
Ivana505 <ivana.iles@protonmail.com>
2021-11-17 17:14:18 +0000

    Add welcome message, list of words for each category and function names for the rest of the game

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
-def example():
-   print("this is example")
-example()
+def welcome_message():
+    print("Good day player ! Try to guess the hidden word to get to your destination.")
+
+game_options()
+choose_game = input ('Choose a category: European cities (press G) or European countries (press Z).')
+if user_choice.upper() == 'G':
+        random(cities)
+elif user_choice.upper() == 'Z':
+       random(cities)
+welcome_message()
+
+
+#def european_cities(cities)
+#print(random.choice(european_cities)) 
+#def european_countries(countries)
+#print(random.choice(european_countries)) 
+
+cities = ['zagreb','dublin','amsterdam','madrid','frankfurt', 'rotterdam', 'reykjavik','budapest','athens','chisinau']
+countries = ['croatia','netherlands','ireland','germany','poland','slovakia','malta','sweeden','greece','france']
+
+def start_game
+
+
+def guessing_letters
+
+def end_game
