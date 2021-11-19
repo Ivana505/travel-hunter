d33c864087a7d3402edb68b3631e3a44d10e4472
Ivana505 <ivana.iles@protonmail.com>
2021-11-19 18:08:56 +0000

    Add game end function

--- run.py
+++ run.py
@@ -1,12 +1,10 @@
 import random
 cities = [
-	'Zagreb', 'Dublin', 'Amsterdam', 'Madrid', 'Frankfurt',
-	'Rotterdam', 'Reykjavik', 'Budapest', 'Athens', 'Chisinau']
+	'ZAGREB', 'DUBLIN', 'AMSTERDAM', 'MADRID', 'FRANKFURT',
+	'ROTTERDAM', 'REYKJAVIK', 'BUDAPEST', 'ATHENS', 'CHISINAU']
 countries = [
-	'Croatia', 'Netherlands', 'Ireland', 'Germany', 'Poland',
-	'Slovakia', 'Malta', 'Sweeden', 'Greece', 'France']
-
-score = 8
+	'CROATIA', 'NETHERLANDS', 'IRELAND', 'GERMANY', 'POLAND',
+	'SLOVAKIA', 'MALTA', 'SWEDEN', 'GREECE', 'FRANCE']
 
 
 def start_game():
@@ -16,7 +14,8 @@ def start_game():
 
 def welcome_message():
 	name = input("What is your name?\n")
-	print(f"Good day {name} ! Try to guess the hidden word to get to your destination.")
+	print(f"Good day {name} ! Try to guess the hidden word to get to your destination. You have 8 tries. ")
+
 
 def choose_game():
 	game_choice = input('Choose a category: European cities (press G) or European countries (press Z).\n').upper()
@@ -30,29 +29,62 @@ def choose_game():
 
 def get_random(word):
 	rand_word = random.choice(word)
-	#print("_ " * len(rand_word))
+	print("_ " * len(rand_word))
 	return rand_word
 
 
 def play_game(word):
 	print(word)
-	letter = []
-	board = ''
-	for character in word:
-		board += '_ '
-	print(board)
+	print("This word is", len(word), "letters long.")
 	while True:
-		guessing = input("Enter letter: \n").upper()
+		guessing = input("Guess a letter: \n").upper()
 		if guessing in word:
 			print("Good guess")
-			board[board.index(guessing)] = guessing
-			print(board)
+			get_guess()
+			game_end()
 			break
 		else:
 			print("Try again !")
 			continue
-	    
 
+
+def get_guess():
+	points = 8
+	guessed = ''
+	valid_entry =set('abcdefghijklmnopqrstuvwz')
+	while len(word) > 0:
+		rand_word = ""
+		missed = 0
+
+	for letter in word:
+		if leter in guessed:
+			rand_word = rand_word + letter
+		else:
+			rand_word = rand_word + "_ "
+			get_guess()
+
+
+
+def game_end():
+	guessed = True
+	while guessed:
+		print = input("Would you like to continue playing the game? y/n ")
+		if start_game() == "y":
+			print("You have decided to continue playing the game.")
+		elif start_game() == "n":
+			print("Now closing the game...")
+			guessed = False
+		else:
+			print("That is not a valid option. Please try again.")
+			print("Thanks for playing")
+
+	
+
+	
+
+
+
+	
 
 
 
@@ -64,13 +96,5 @@ start_game()
 
 
 
+#check_guess():
 
-
-#while letter in rand_word:
-	#print("Great, keep guessing")
-#if letter not in rand_word:
-#	break
-	#print("Try again")
-
-	
-# def end_game
