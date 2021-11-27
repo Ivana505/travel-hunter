4f91a125968ca76c249bc42bec893e3818f164e0
Ivana505 <ivana.iles@protonmail.com>
2021-11-27 23:20:05 +0000

    Add docstring to code and removed problems from the code

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-import pyfiglet
 import random
 import sys
+import pyfiglet
 cities = [
     'AMSTERDAM', 'ANDORRA LA VELLA', 'ATHENS', 'BELGRADE', 'BERLIN', 'BERN',
     'BRATISLAVA', 'BRUSSELS', 'BUCHAREST', 'BUDAPEST', 'CHISINAU',
@@ -21,11 +21,20 @@ countries = [
 
 
 def start_game():
+    """
+   Starts game and calls welcome message function
+   first and then choose game function for the
+   player to choose type of the game.
+    """
     welcome_message()
     choose_game()
 
 
 def welcome_message():
+    """
+   Welcome message function with explanation
+   how the game is played.
+    """
     print(pyfiglet.figlet_format("Welcome to travel hunter", justify="center"))
     while True:
         name = input("What is your name?\n")
@@ -40,6 +49,12 @@ def welcome_message():
 
 
 def choose_game():
+    """
+   Gives an option to choose between two type of games.
+   Isalpha is making sure player chooses letter.
+   Else statement is making sure game does not start
+   until correct letter has been chose either G or Z.
+    """
     print("Choose a category.")
     game_choice = input(
         "European cities (press G) or European countries (press Z).\n").upper()
@@ -55,33 +70,40 @@ def choose_game():
     choose_game()
 
 
-
-# def enter_check():
-#     if len(guess) != 1:
-#         print('Please enter a single letter.')
-#     elif guess in guessed_letters:
-#         print('You have already guessed that letter. Choose again.')
-    
-
-
 def enter_check():
-    inputChar = input("Enter single character: ")
-    
-    while True:
-        if inputChar.lower() in "abcdefghijklmnopqrstuvwxyz" and len(inputChar) == 1:
-            break
-        
-        inputChar = input("Enter single character: ")
+    """
+   While true loop is making sure only one single character,
+   in this case letter has been chosen by the player.
+    """
+    input_char = input("Enter single character: ")
 
-    return inputChar
+    while True:
+        if (input_char.lower() in "abcdefghijklmnopqrstuvwxyz"
+           and len(input_char) == 1):
+            break
+
+        input_char = input("Enter single character: ")
+
+    return input_char
 
 
 def get_random(word):
+    """
+    Function which returns the random
+    word from the array.
+    """
     rand_word = random.choice(word)
     return rand_word
 
 
 def play_game(rand_word):
+    """
+    Main function of the game which scans if the letter that is in
+    the random word is guessed or not. If guessed letter will show
+    if not guessed attempts will reduce by 1. If the word is
+    guessed player wins, if not word is revealed. In both scenarios
+    player is passed to game end function.
+    """
     attempts = 8
     guessed_letters = []
     done = False
@@ -90,7 +112,7 @@ def play_game(rand_word):
             if letter.upper() in guessed_letters:
                 print(letter, end=" ")
             else:
-                    print("_ ", end=" ")
+                print("_ ", end=" ")
         print("")
         print(f"\nAmount of the attempts left {attempts}, Next Guess \n")
         guess = enter_check()
@@ -115,8 +137,11 @@ def play_game(rand_word):
             game_end()
 
 
-
 def game_end():
+    """
+    Asks if player wants to continue playing the game or not.
+    After Y or N message is displayed.
+    """
     again = input("Do you want to play again press Y or N ?\n").upper()
     if again == 'Y':
         print(" Yes is a good answer, enjoy playing again ! ")
@@ -130,6 +155,11 @@ def game_end():
 
 
 def goodbye_message():
+    """
+   Final function which ends the game if the
+   player pressed N to end the game.
+   It also prints Goodbye message with pyfiglet.
+    """
     print(pyfiglet.figlet_format("Goodbye ! ", justify="center"))
     sys.exit()
 
